64dae69c3335392cafea51bf86e191dc5e7c4e44
Ludovic Logiou <ludovic.logiou@objectif-libre.com>
2018-02-21 21:09:48 +0100

    Add entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ $1 = "bash" ]; then
-	if [ -e /bin/bash ]; then
-		/bin/bash
-	else
-		/bin/sh
-	fi
+TRANSMISSION_USER="${TRANSMISSION_USER:-"transmission"}"
+TRANSMISSION_PASSWORD="${TRANSMISSION_PASSWORD:-"transmission"}"
+TRANSMISSION_DIR="${TRANSMISSION_DIR:-"/var/lib/transmission"}"
+if [ $TRANSMISSION_PASSWORD == "transmission" ]; then
+	echo "Warning you use default password, you should change it"
 fi
+exec /usr/bin/transmission-daemon -f --no-portmap --log-error --config-dir /etc/transmission-daemon/ --username "$TRANSMISSION_USER" --password "$TRANSMISSION_PASSWORD"  --download-dir "$TRANSMISSION_DIR" --incomplete-dir "$TRANSMISSION_INCOMPLETE" --bind-address-ipv4 "0.0.0.0" --auth
